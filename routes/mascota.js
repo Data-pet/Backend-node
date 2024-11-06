1fb3a3f5fd18618b8dfc174d8129911aa02dcc2a
Bruno Ignacio Bordón <118336756+bruno10-0@users.noreply.github.com>
2024-11-05 22:04:19 -0300

    Agregando nuevo controlador para traer las mascotas del usuario logueado

--- routes/mascota.js
+++ routes/mascota.js
@@ -5,10 +5,12 @@ import {
   createMascota,
   updateMascota,
   deleteMascota,
+  getMascotasUsuario,
 } from "../controllers/mascotas/mascotaController.js";
 const router = Router();
 
 router.get("/mascotas", getMascotas);
+router.get("/mascotasUsuario/:idUsuario", getMascotasUsuario);
 router.get("/mascotas/:id", getMascotaById);
 router.post("/mascotas", createMascota);
 router.put("/mascotas/:id", updateMascota);
